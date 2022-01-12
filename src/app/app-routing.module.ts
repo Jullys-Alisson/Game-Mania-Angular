ab0b106309304def2ad48aeae848fd021cea00b0
Jullys Alisson lima <you@example.comjullysalisson10@gmail.comgit config --global user.name Your>
2022-01-12 11:00:17 -0300

    Projeto Angular v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarrinhoComponent } from './views/carrinho/carrinho.component';
+import { ContatoComponent } from './views/contato/contato.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { LojaComponent } from './views/loja/loja.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"", component: HomeComponent },
+  { path: "contato", component: ContatoComponent},
+  { path: "loja", component: LojaComponent},
+  { path: "carrinho", component: CarrinhoComponent},
+  { path: "login", component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { ContatoComponent } from './views/contato/contato.component';
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LojaComponent } from './views/loja/loja.component';
+import { CarrinhoComponent } from './views/carrinho/carrinho.component';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContatoComponent,
+    HeaderComponent,
+    FooterComponent,
+    LojaComponent,
+    CarrinhoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
